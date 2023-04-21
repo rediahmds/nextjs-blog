ab4c9e40c87476e5fdc6aa67feb0be26b96131b2
Redi Ahmad <redi.ngoding@gmail.com>
2023-04-21 10:49:12 +0700

    Add Head element

--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -1,9 +1,13 @@
 import React from 'react';
 import Link from 'next/link';
+import Head from 'next/head';
 
 export default function FirstPost() {
   return (
     <div>
+      <Head>
+        <title>First Post</title>
+      </Head>
       <h1>First Post</h1>
       <div>
         <Link href="/">Return Home</Link>
